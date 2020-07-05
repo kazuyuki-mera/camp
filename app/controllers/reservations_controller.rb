1fb6fab7849f3bf05b4c138be05bd6811f143388
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-05 19:09:21 +0900

    予約機能・予約画面作成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,13 @@
 class ReservationsController < ApplicationController
+  # ユーザーのログインチェック
+  before_action :require_login
+
   # 新規作成画面
   def new
-    if user_signed_in?
-      @product = Product.find(params[:product_id])
-      @reservation = Reservation.new
-    else
-      flash[:notice] = '予約には会員登録もしくはログインが必要です。'
-      redirect_to new_user_registration_path # ユーザー新規作成画面
-    end
+    # 商品情報を取得
+    @product = Product.find(params[:product_id])
+    # 予約情報を初期化
+    @reservation = Reservation.new
   end
 
   # 作成機能
@@ -17,12 +17,30 @@ class ReservationsController < ApplicationController
 
   # 詳細画面
   def show
+    # TODO: 以下の取得条件を修正する
     @reservation = Reservation.find(1)
+    if current_user.id == @reservation.user.id
+      edirect_to redirect_to new_user_registration_path
+    end
   end
 
   ######################### private #########################
   private
+    # reservationストロングパラメータ
     def reservation_params
       params.require(:reservation).permit(:start_date, :end_date, :reserver_count).merge(product_id: params[:product_id], user_id: current_user.id)
+    end
+
+    # ユーザーが未ログインの場合、会員登録画面へリダイレクト
+    def require_login
+      unless user_signed_in?
+        flash[:notice] = '予約には会員登録もしくはログインが必要です。'
+        redirect_to new_user_registration_path # ユーザー新規作成画面
+      end
+    end
+
+    # 宿泊開始終了日のFromToチェック
+    def from_to_check
+      
     end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,4 +6,11 @@ class Reservation < ApplicationRecord
   def start_end_date
     self.start_date.strftime("%Y年%m月%d日") + ' 〜 ' + self.end_date.strftime("%m月%d日")
   end
+
+  def select_reserver_count(max_count)
+    select_box = []
+    for num in 1..max_count do
+      select_box << num
+    end
+  end
 end
