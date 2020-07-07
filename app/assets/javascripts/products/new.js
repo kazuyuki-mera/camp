a32f4b21952e81e5f7b35c12f20e4f16fb81fe90
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-07 21:27:10 +0900

    商品編集画面レイアウト修正

--- app/assets/javascripts/products/new.js
+++ app/assets/javascripts/products/new.js
@@ -1,4 +1,5 @@
 $(function(){
+
   $('#product_image').change(function(){
     let viewArea = $('.view-area');
     let inputFile = $(this).prop('files')[0];
@@ -10,24 +11,19 @@ $(function(){
       inputImg.remove();
     }
 
-    let img = '<img width="100%" class="img-view"><a href="#" class="img-delete">画像を削除する</a>';
+    let img = $('#product-image');
     viewArea.append(img);
 
     fileRead.onload = function(){
       viewArea.find('img').attr('src', fileRead.result);
-      deleteimg(viewArea);
     }
     fileRead.readAsDataURL(inputFile);
   });
 
-  function deleteimg(target){
-    target.find('a.img-delete').on('click', function(){
-      if(window.confirm('画像を削除します。\nよろしいですか？')){
-        $(this).parent().find('input[type=file]').val('');
-        $(this).parent().find('.img-view, br').remove();
-        $(this).remove();
-      }
-      return false;
-    });
-  }
+  $('#product-image').click(function(){
+    if (confirm('画像を削除してよろしいでしょうか？')) {
+      $(this).attr('src', '');
+      $('#product_image').val('');
+    }
+  });
 });
