bb56a4556c3241b4be8069a098b6eea4d92c909c
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-08 09:39:58 +0900

    productモデルにバリデーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
   # アソシエーション
   has_many :reservations
-  mount_uploader :image, ImageUploader  
+
+  # バリデーション
+  validates :name,      presence: true, length: { maximum: 20 }
+  validates :price,     presence: true, numericality: { only_integer: true, less_than: 999999 }
+  validates :content,   presence: true, length: { maximum: 200 }
+  validates :max_count, presence: true, numericality: { only_integer: true, less_than: 999 }
+
+  mount_uploader :image, ImageUploader
 end
