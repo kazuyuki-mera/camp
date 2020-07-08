22a596fd21306e0d8eb6ca68214a3c4299112537
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-08 23:07:03 +0900

    コミット漏れ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,7 +36,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to product_path(product)
+      redirect_to product_path(@product)
     else
       render :edit
     end
