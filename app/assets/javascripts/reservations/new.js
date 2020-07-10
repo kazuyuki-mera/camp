d04ede660a13bd42f99a6d2d8f82c7dd59d553d1
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-11 00:21:26 +0900

    予約画面宿泊料金計算JavaScript追加

--- app/assets/javascripts/reservations/new.js
+++ app/assets/javascripts/reservations/new.js
@@ -1,9 +1,41 @@
 $(function(){
+  // 予約確認ダイアログ
   $('#reservation-confirm').click(function(){
     if (!confirm('この内容で予約してよろしいでしょうか？')) {
       return false;
     } else {
       $('p').text('予約が完了しました。');
     }
-  })
+  });
+
+  // 宿泊開始日変更メソッド
+  $('#reservation_start_date').change(function(){
+    let startDate = new Date($('#reservation_start_date').val());
+    let endDate = new Date($('#reservation_end_date').val());
+    let totalPrice = calcPrice(startDate, endDate);
+    $('#total_price').text(totalPrice + '円');
+    $('#reservation_total_price').val(totalPrice);
+  });
+
+  // 宿泊終了日変更メソッド
+  $('#reservation_end_date').change(function(){
+    let startDate = new Date($('#reservation_start_date').val());
+    let endDate = new Date($('#reservation_end_date').val());
+    let totalPrice = calcPrice(startDate, endDate);
+    $('#total_price').text(totalPrice + '円');
+    $('#reservation_total_price').val(totalPrice);
+  });
+
+  // 宿泊料金計算
+  function calcPrice(startDate, endDate) {
+    let diffDate = (endDate - startDate) / 86400000;
+    const reservationPrice = $('#hidde-price').data('price');
+    if (diffDate == 0) {
+      return reservationPrice.toString();
+    } else if (diffDate > 0) {
+      return (reservationPrice * diffDate).toString();
+    } else {
+      return '0';
+    }
+  }
  });
