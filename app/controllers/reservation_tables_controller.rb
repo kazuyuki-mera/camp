8412817c488fd0b437937993d3f343d76f3edd2f
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2020-07-23 10:51:15 +0900

    予約一覧・検索において管理者判定を追加

--- app/controllers/reservation_tables_controller.rb
+++ app/controllers/reservation_tables_controller.rb
@@ -1,4 +1,6 @@
 class ReservationTablesController < ApplicationController
+  # 管理者のみアクセス許可
+  before_action :authenticate_admin
   def index
     @reservations = ReservationTable.where(reservation_date: Date.today)
   end
@@ -7,4 +9,10 @@ class ReservationTablesController < ApplicationController
     @reservation_date = Date.strptime(params[:reservation_date],'%Y-%m-%d')
     @reservations = ReservationTable.where(reservation_date: params[:reservation_date].to_date)
   end
+
+  private
+    # 管理者判定
+    def authenticate_admin
+      redirect_to products_path unless user_signed_in? && current_user.admin?
+    end
 end
